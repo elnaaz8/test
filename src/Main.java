a42bfa9f5652ecd64298876ee2903c9ef73be886
elnaaz8 <elnazbolatbekova@gmail.com>
2024-02-13 11:20:15 +0600

    -

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,12 @@ public class Main {
     public void testSecurity() {
     }
 
-    // Тестирование визуального интерфейса
     public void testVisualInterface() {
     }
 
-    // Тестирование сообщений об ошибках
     public void testErrorMessages() {
     }
 
-    // Запуск всех тестов
     public static void main(String[] args) {
         Main loginPageTest = new Main();
         loginPageTest.testInputFunctionality();
