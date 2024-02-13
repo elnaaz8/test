1d9a7eb7fd9ec6136d2ff077a7dbb6ad79b0ad1e
elnaaz8 <elnazbolatbekova@gmail.com>
2024-02-13 11:06:52 +0600

    -

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    public void testInputFunctionality() {
+    }
+
+    public void testSecurity() {
+    }
+
+    // Тестирование визуального интерфейса
+    public void testVisualInterface() {
+    }
+
+    // Тестирование сообщений об ошибках
+    public void testErrorMessages() {
+    }
+
+    // Запуск всех тестов
+    public static void main(String[] args) {
+        Main loginPageTest = new Main();
+        loginPageTest.testInputFunctionality();
+        loginPageTest.testSecurity();
+        loginPageTest.testVisualInterface();
+        loginPageTest.testErrorMessages();
     }
 }
+
